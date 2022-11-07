3b41947aa82e2d98e2becc93352c10a1a8d551db
LuisVallada <valladaluis@gmail.com>
2022-11-07 17:05:10 -0300

    Código de entrada

--- codigo.py
+++ codigo.py
@@ -1,7 +1,16 @@
+from funcoes import * 
+
+
 
 nome = input('Qual seu nome? ')
-
-# ola = 'Ok ' + str(nome) +', ' + 'você ' + 'tem '+ 'direito ' + 'a ' + 'pular ' + '3 ' + 'vezes ' + 'e ' + '2 ' + 'ajudas!'
-# print(ola)
-
+print('\n')
 print(f'Ok {nome}, você tem direito a pular 3 vezes e 2 ajudas!')
+print('\n')
+print('As opcções de resposta são  ''A'',''B'', ''C'', ''D'', ''ajuda'', ''pula'' e ''parar''!')
+print('\n')
+enter = input('Aperte ENTER para continuar')
+print('\n')
+print('O jogo já vai começar! Lá vem a primeira questão!')
+print('\n')
+print('Vamos começar com o nível FÁCIL!')
+enter = input('Aperte ENTER para continuar')
