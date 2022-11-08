9ae2dbc49816a59b2c0d434e0992f39764caf0b3
melinastergiou <melina.belli.stergiou@gmail.com>
2022-11-07 23:50:17 -0300

    iniciando codigo, peguntas e resposta correta

--- codigo.py
+++ codigo.py
@@ -3,25 +3,83 @@ from base_dados import quest
 
 dic = transforma_base(quest)
 
-
-
+ajudas = 2
+pular = 3
+pontos = 0
+premios = [1000, 5000, 10000, 30000, 50000, 100000, 300000, 500000, 1000000]
+niveis= ['facil', 'medio', 'dificil']
 nome = input('Qual seu nome? ')
+
+pergunta = 1 # número das questões
+sorteadas = [] # lista das perguntas já sorteadas
+
 print('\n')
 print(f'Ok {nome}, você tem direito a pular 3 vezes e 2 ajudas!')
 print('\n')
 print('As opcções de resposta são  "A", "B", "C", "D", "ajuda", "pula" e "parar"!')
 print('\n')
 enter = input('Aperte ENTER para continuar...')
-print('\n')
-print('O jogo já vai começar! Lá vem a primeira questão!')
-print('\n')
-print('Vamos começar com o nível FÁCIL!')
-enter = input('Aperte ENTER para continuar...')
-print('\n')
-print('\n')
-print('------------------------------------------------')
-print('\n')
-print('QUESTÃO 1')
 
-# # indentificar nível anftes 
-# sorteia_questao(dic,nivel)
+
+if pergunta < 3:
+    nivel = niveis[0]
+    print('\n')
+    print('O jogo já vai começar! Lá vem a primeira questão!')
+    print('\n')
+    print(f'Vamos começar com o nível {nivel}')
+    enter = input('Aperte ENTER para continuar...')
+    sorteio = sorteia_questao_inedida(dic,nivel,sorteadas)
+    print(sorteio)
+   #  
+    questao= sorteio['titulo']
+    opcoes = questao_para_texto(sorteio, pergunta)
+    pergunta +=1
+    correta = sorteio['correta']
+
+    print(questao)
+    print(opcoes)
+
+    resposta = input('\nQual a sua resposta? ')
+
+    if resposta ==correta:
+        premio=premios[pontos]
+        print(f'Você acertou! Seu prêmio atual é de {premio}!')
+        enter = input('Aperte ENTER para continuar...')
+    # else:
+    #     while resposta =='ajuda':
+    #         ajudas-=1
+
+    #     while resposta =='pular':
+    #         pular-=1
+
+    #     if resposta=='parar':
+
+    
+
+# elif pergunta >3 and pergunta<6:
+#     nivel=niveis[1]
+#     print(f'Hey! Você passou para no nível {nivel}')
+#     enter = input('Aperte ENTER para continuar...')
+
+#     sorteio = sorteia_questao_inedida(dic,nivel,sorteadas)
+    
+#     questao= sorteio['titulo']
+#     opcoes = sorteio['opcoes']
+
+# else:
+#     nivel = niveis[2]
+#     print(f'Hey! Você passou para no nível {nivel}')
+#     enter = input('Aperte ENTER para continuar...')
+
+    
+#     sorteio = sorteia_questao_inedida(dic,nivel,sorteadas)
+    
+    
+#     pergunta= sorteio['titulo']
+#     opcoes = sorteio['opcoes']
+
+# print('\n')
+# print('\n')
+# print('------------------------------------------------')
+# print('\n')
+
